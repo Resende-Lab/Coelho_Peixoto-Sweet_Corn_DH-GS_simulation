cb65a0cd5965dbb8f4aaa659961a9c55a9657ff8
Marco Antônio Peixoto <59318360+marcopxt@users.noreply.github.com>
2024-10-16 09:30:26 -0300

    Delete 1.Parental_selection directory

--- 1.Parental_selection/UpdateParents_ConvOver.R
+++
@@ -1,9 +0,0 @@
-###---------
-# Choosing the best lines to update the crossing blocks
-# Strategy Conventional overlapping
-###---------
-
-
-Parents_public_update = c(F5_public.sel,F3_public.sel,Parents_public_update)
-Parents_public_update = selectInd(Parents_public_update, nInd= nParents_public, selectTop = TRUE)
-
--- 1.Parental_selection/UpdateParents_GSDis.R
+++
@@ -1,9 +0,0 @@
-###---------
-# Choosing the best lines to update the crossing blocks
-# Strategy Genomic selection Discrete
-###---------
-
-
-Parents_public_update = c(F5_public.sel,F3_public.sel)
-Parents_public_update = selectInd(Parents_public_update, nInd= nParents_public, selectTop = TRUE, use="ebv")
-
