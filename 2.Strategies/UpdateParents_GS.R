3eda75fd8a7d78de27ad290f0833ed89a063e9fb
Marco Antônio Peixoto <59318360+marcopxt@users.noreply.github.com>
2024-10-16 09:30:38 -0300

    Delete 2.Strategies directory

--- 2.Strategies/UpdateParents_GS.R
+++
@@ -1,7 +0,0 @@
-###---------
-# Choosing the best lines to update the crossing blocks
-###---------
-
-Parents_public_update = c(F5_public.sel,F3_public.sel)
-Parents_public_update = selectInd(Parents_public_update, nInd= nParents_public, selectTop = TRUE, use="ebv")
-
--- 2.Strategies/UpdateParents_public.R
+++
@@ -1,5 +0,0 @@
-#Replace oldest hybrid parent with parent of best inbreds
-
-Parents_public_update = c(F7_public.sel,F6_public.sel)
-Parents_public_update = selectInd(Parents_public_update, nInd= nParents_public, selectTop = TRUE)
-
